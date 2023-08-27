efb3828e55207c8b241a62759c8665c042186c30
이남곤 <nglee.dev@gmail.com>
2023-08-27 22:15:05 +0900

    1부: JUnit5 Assertions

--- src/test/java/com/thejavatest/StudyTest.java
+++ src/test/java/com/thejavatest/StudyTest.java
@@ -1,10 +1,18 @@
 package com.thejavatest;
 
+import jdk.jfr.Enabled;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DisplayName("스터디 테스트")
@@ -38,6 +46,25 @@ class StudyTest {
             Thread.sleep(300);
         });
         // TODO ThreadLocal를 사용하는 경우, 예상치 못한 결과가 나타날 수도 있다.
+    }
+
+    @Test
+//    @EnabledOnOs(OS.MAC)
+    @EnabledOnOs({ OS.MAC, OS.LINUX })
+    @EnabledOnJre({ JRE.JAVA_8, JRE.JAVA_17 })
+    void localTest() {
+        String testEnv = System.getenv("TEST_ENV");
+        System.out.println(testEnv);
+        assumeTrue("LOCAL".equalsIgnoreCase(testEnv));
+        assumingThat("LOCAL".equalsIgnoreCase(testEnv), () -> {
+
+        });
+        assumingThat("PROD".equalsIgnoreCase(testEnv), () -> {
+
+        });
+
+        Study study = new Study(10);
+        assertThat(study.getLimit()).isGreaterThan(0);
     }
 
     @Test
