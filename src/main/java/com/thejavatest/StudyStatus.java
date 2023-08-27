ad4fbc533ffb9db0882f5c5a788a2190a57d57bd
이남곤 <nglee.dev@gmail.com>
2023-08-27 21:48:59 +0900

    1부: JUnit5 Assertions

---
+++ src/main/java/com/thejavatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.thejavatest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/com/thejavatest/StudyTest.java
+++ src/test/java/com/thejavatest/StudyTest.java
@@ -2,6 +2,8 @@ package com.thejavatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -11,9 +13,31 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+//        Study study = new Study(-10);
+//        assertNotNull(study);
+//        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
+//        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 " + StudyStatus.DRAFT + " +여야 한다.");
+//        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
+
+//        assertAll(
+//                () -> assertNotNull(study),
+//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
+//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 " + StudyStatus.DRAFT + " +여야 한다."),
+//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+//        );
+
+//        assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+
+//        assertTimeout(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
+
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+        // TODO ThreadLocal를 사용하는 경우, 예상치 못한 결과가 나타날 수도 있다.
     }
 
     @Test
