5d990d90dbf36e31af253cf2463d54eb2f3b2720
이남곤 <nglee.dev@gmail.com>
2023-09-04 01:56:39 +0900

    2부: Mock 객체 Stubbing

--- src/test/java/com/thejavatest/study/StudyServiceTest.java
+++ src/test/java/com/thejavatest/study/StudyServiceTest.java
@@ -1,11 +1,18 @@
 package com.thejavatest.study;
 
+import com.thejavatest.domain.Member;
 import com.thejavatest.member.MemberService;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -18,8 +25,24 @@ class StudyServiceTest {
     @Test
     void createStudyService(@Mock MemberService memberService,
                             @Mock StudyRepository studyRepository) {
+
         StudyService studyService = new StudyService(memberService, studyRepository);
-        Assertions.assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("keesun@email.com");
+
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+        Optional<Member> findById = memberService.findById(1L);
+        Assertions.assertEquals("keesun@email.com", findById.get().getEmail());
+
+        when(memberService.findById(1L)).thenThrow();
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+        memberService.validate(1L);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
     }
 
 }
